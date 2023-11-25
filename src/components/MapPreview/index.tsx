472282ca94cb62ee6e70128d6fd2345a15d9f935
omiddd <omidrazzaghi2000@gmail.com>
2023-11-25 13:56:11 +0330

    fix map bug and added time line former config

--- src/components/MapPreview/index.tsx
+++ src/components/MapPreview/index.tsx
@@ -1,11 +1,12 @@
 import { MapContainer, TileLayer, useMap } from 'react-leaflet'
-import "./leaflet.css"
+import 'leaflet/dist/leaflet.css';
+
 export function MapPreview() {
 
   
 
   return (
-<MapContainer  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} attributionControl={false}>
+<MapContainer style={{height : '100vh',width:'100vw'}}  center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true} attributionControl={false}>
   <TileLayer
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
